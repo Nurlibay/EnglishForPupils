e5ed594d73858b0e1b07d7bd01a7c6e47b73c89e
AtabekNazarbaev <atabeknazarbaev441@gmail.com>
2023-03-29 21:11:08 +0500

    antonym data added

--- app/src/main/java/uz/nurlibaydev/englishforpupils/data/DataList.kt
+++ app/src/main/java/uz/nurlibaydev/englishforpupils/data/DataList.kt
@@ -777,6 +777,146 @@ object DataList {
                     "traditional",
                 )
             }
+            11 -> {
+                return mutableListOf(
+                    "Get ill",
+                    "Patient",
+                    "Disease",
+                    "live",
+                    "Agree",
+                    "Now",
+                    "Introduction",
+                    "Early",
+                    "Possible",
+                    "Can"
+                )
+            }
+            12 -> {
+                return mutableListOf(
+                    "Harmful",
+                    "Pretty",
+                    "Rare",
+                    "Less",
+                    "Mental",
+                    "Possible",
+                    "Improve",
+                    "Reduce",
+                    "Natural",
+                    "Turn on"
+                )
+            }
+            13 -> {
+                return mutableListOf(
+                    "Brave",
+                    "Pleasant",
+                    "Relaxed",
+                    "Energetic",
+                    "Deep",
+                    "Distant",
+                    "Top",
+                    "True",
+                    "Safe",
+                    "Modern"
+                )
+            }
+            14 -> {
+                return mutableListOf(
+                    "Tidy",
+                    "Dirty",
+                    "Healthy",
+                    "Modern",
+                    "Pessimist",
+                    "Straight",
+                    "Order",
+                    "Loner",
+                    "Active",
+                    "Logical"
+                )
+            }
+            15 -> {
+                return mutableListOf(
+                    "Good",
+                    "Nice",
+                    "Happy",
+                    "Sad",
+                    "Big",
+                    "Little",
+                    "Walk",
+                    "Scared",
+                    "Beautiful",
+                    "Like"
+                )
+            }
+            16 -> {
+                return mutableListOf(
+                    "Danger",
+                    "Acquaintance",
+                    "Basic",
+                    "First",
+                    "Together",
+                    "Wild",
+                    "Straight",
+                    "Skillful",
+                    "Proud",
+                    "Male"
+                )
+            }
+            17 -> {
+                return mutableListOf(
+                    "Color",
+                    "Painful",
+                    "Absent",
+                    "All",
+                    "Above",
+                    "Backward",
+                    "Separate",
+                    "External",
+                    "Export",
+                    "Frequent"
+                )
+            }
+            18 -> {
+                return mutableListOf(
+                    "Forget",
+                    "Freedom",
+                    "Loyal",
+                    "Mature",
+                    "Understand",
+                    "Increase",
+                    "Accept",
+                    "General",
+                    "Domestic",
+                    "Open"
+                )
+            }
+            19 -> {
+                return mutableListOf(
+                    "Accurate",
+                    "Borrow",
+                    "Entrance",
+                    "Horizontal",
+                    "Intentional",
+                    "Ordinary",
+                    "Attack",
+                    "Rural",
+                    "Arrival",
+                    "Center"
+                )
+            }
+            20 -> {
+                return mutableListOf(
+                    "South",
+                    "Fix",
+                    "Stationary",
+                    "Beneath",
+                    "Bald",
+                    "Catch",
+                    "Even",
+                    "Collect",
+                    "Obey",
+                    "Complicated"
+                )
+            }
         }
         return mutableListOf()
     }
@@ -922,6 +1062,146 @@ object DataList {
                     "take out",
                     "local",
                     "modern"
+                )
+            }
+            11 -> {
+                return mutableListOf(
+                    "recover",
+                    "doctor",
+                    "remedy",
+                    "die",
+                    "disagree",
+                    "then",
+                    "conclusion",
+                    "late",
+                    "impossible",
+                    "not be able to"
+                )
+            }
+            12 -> {
+                return mutableListOf(
+                    "harmless",
+                    "not attractive",
+                    "frequent",
+                    "more",
+                    "physical",
+                    "impossible",
+                    "worsen",
+                    "increase",
+                    "artificial",
+                    "turn off",
+                )
+            }
+            13 -> {
+                return mutableListOf(
+                    "fearful",
+                    "disappointed",
+                    "worried",
+                    "tired",
+                    "Shallow",
+                    "Near",
+                    "Bottom",
+                    "False",
+                    "Dangerous",
+                    "Traditional"
+                )
+            }
+            14 -> {
+                return mutableListOf(
+                    "messy",
+                    "clean",
+                    "sick",
+                    "ancient",
+                    "optimist",
+                    "curly",
+                    "disorder",
+                    "joiner",
+                    "passive",
+                    "illogical"
+                )
+            }
+            15 -> {
+                return mutableListOf(
+                    "nice",
+                    "enjoyable",
+                    "cheerful",
+                    "depressed",
+                    "large",
+                    "small",
+                    "stroll",
+                    "afraid",
+                    "gorgeous",
+                    "admire"
+                )
+            }
+            16 -> {
+                return mutableListOf(
+                    "safety",
+                    "stranger",
+                    "inessential",
+                    "last",
+                    "apart",
+                    "domesticated",
+                    "crooked",
+                    "unskillful",
+                    "humble",
+                    "female"
+                )
+            }
+            17 -> {
+                return mutableListOf(
+                    "colorless",
+                    "painless",
+                    "present",
+                    "none",
+                    "below",
+                    "forward",
+                    "combine",
+                    "internal",
+                    "import",
+                    "seldom"
+                )
+            }
+            18 -> {
+                return mutableListOf(
+                    "remember",
+                    "captivity",
+                    "disloyal",
+                    "immature",
+                    "misunderstand",
+                    "decrease",
+                    "reject",
+                    "particular",
+                    "wild",
+                    "shut"
+                )
+            }
+            19 -> {
+                return mutableListOf(
+                    "inaccurate",
+                    "lend",
+                    "exit",
+                    "vertical",
+                    "accidental",
+                    "unusual",
+                    "defend",
+                    "urban",
+                    "departure",
+                    "outskirts"
+                )
+            }
+            20 -> {
+                return mutableListOf(
+                    "north",
+                    "break",
+                    "movable",
+                    "above",
+                    "hairy",
+                    "throw",
+                    "odd",
+                    "distribute",
+                    "disobey",
+                    "simple"
                 )
             }
         }
